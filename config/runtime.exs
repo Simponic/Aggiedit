c9a6abf9b7f67dd1bdf465de02fe17e859914464
Logan Hunt <loganhunt@simponic.xyz>
2022-04-21 18:54:26 -0600

    Get ready for deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -32,6 +32,16 @@ if config_env() == :prod do
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
+  config :aggiedit, Aggiedit.Mailer,
+    adapter: Swoosh.Adapters.SMTP,
+    relay: System.get_env("SMTP_SERVER"),
+    port: 587,
+    username: System.get_env("CONTACT_EMAIL"),
+    password: System.get_env("EMAIL_PASSWORD"),
+    tls: :always,
+    auth: :always,
+    no_mx_lookups: true
+
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\aggiedit" eval Aggiedit.Release.migrate
